02e53ce06e7a56180becf8c78189b8a8b981568f
DESKTOP-1QRG0GO\AB <batyhtik3@gmail.com>
2025-02-07 01:12:38 +0300

    remove database and efcore

---
+++ AATelegramBotMusic.DB/Repositories/LocalMusicRepository.cs
@@ -0,0 +1,74 @@
+﻿using AATelegramBotMusic.DB.Entities;
+
+namespace AATelegramBotMusic.DB.Repositories
+{
+    public class LocalMusicRepository : IMusicRepository
+    {
+        private readonly List<Music> _musics = new();
+
+        public Task<bool> Create(Music? music)
+        {
+            if (music is null)
+            {
+                return Task.FromResult(false);
+            }
+
+            _musics.Add(music);
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> Update(Music? music)
+        {
+            if (music is null)
+            {
+                return Task.FromResult(false);
+            }
+
+            var index = _musics.FindIndex(x => x.TgMessageId == music.TgMessageId);
+            if (index == -1) return Task.FromResult(false);
+
+            _musics[index] = music;
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> Delete(Music? music)
+        {
+            if (music is null)
+            {
+                return Task.FromResult(false);
+            }
+
+            return Task.FromResult(_musics.Remove(music));
+        }
+
+        public Task<Music?> GetNotApproved(int messageId)
+        {
+            return Task.FromResult(_musics.FirstOrDefault(x => x.TgMessageId == messageId && !x.IsApproved));
+        }
+
+        public Task<Music?> GetApproved(int messageId)
+        {
+            return Task.FromResult(_musics.FirstOrDefault(x => x.TgMessageId == messageId && x.IsApproved));
+        }
+
+        public Task<List<Music>> GetNotApproved(string? mediaGroupId)
+        {
+            return Task.FromResult(_musics.Where(x => x.MediaGroupId == mediaGroupId && !x.IsApproved).ToList());
+        }
+
+        public Task<List<Music>> GetApproved(string? mediaGroupId)
+        {
+            return Task.FromResult(_musics.Where(x => x.MediaGroupId == mediaGroupId && x.IsApproved).ToList());
+        }
+
+        public Task<Music?> Get(int messageId)
+        {
+            return Task.FromResult(_musics.FirstOrDefault(x => x.TgMessageId == messageId));
+        }
+
+        public Task<List<Music>> Get(string? mediaGroupId)
+        {
+            return Task.FromResult(_musics.Where(x => x.MediaGroupId == mediaGroupId).ToList());
+        }
+    }
+}
--- AATelegramBotMusic/Program.cs
+++ AATelegramBotMusic/Program.cs
@@ -1,9 +1,7 @@
 ﻿using AATelegramBotMusic.Converter;
-using AATelegramBotMusic.DB;
 using AATelegramBotMusic.DB.Repositories;
 using AATelegramBotMusic.Ftp;
 using AATelegramBotMusic.Music;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,7 +14,6 @@ namespace AATelegramBotMusic
             var configBuilder = new ConfigurationBuilder()
                  .AddJsonFile($"appsettings.json", true, true);
             var config = configBuilder.Build();
-            var connection = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("DefaultConnection is null!");
             var token = config["TelegramBotToken"] ?? throw new InvalidOperationException("TelegramBotToken is null!");
             var targetChatId = long.Parse(config["TargetChatId"] ?? throw new InvalidOperationException("TargetChatId is null!"));
             var targetThreadId = int.Parse(config["TargetThreadId"] ?? throw new InvalidOperationException("TargetThreadId is null!"));
@@ -24,14 +21,10 @@ namespace AATelegramBotMusic
             var isWelcomeMessage = bool.Parse(config["IsWelcomeMessage"] ?? throw new InvalidOperationException("IsWelcomeMessage is null!"));
 
             var services = new ServiceCollection()
-                .AddDbContext<ApplicationDbContext>(options =>
-                {
-                    options.UseSqlServer(connection);
-                })
                 .AddSingleton<TelegramBot>()
                 .AddSingleton<IMusicConverter, FFMpegMusicConverter>()
                 .AddSingleton<IMusicService, MusicService>()
-                .AddScoped<IMusicRepository, MusicRepository>()
+                .AddSingleton<IMusicRepository, LocalMusicRepository>()
                 .AddScoped<IFtpService, FtpService>()
                 .BuildServiceProvider();
             await services.GetRequiredService<TelegramBot>().Start(token, admins, isWelcomeMessage, targetChatId, targetThreadId);
